90b92a67685fa72d52294a275cb8faf8ccaa4610
sigmadruid <sigmadruid@qq.com>
2022-03-20 16:45:29 +0800

    better editor preview

---
+++ Assets/Scripts/Editor/TerrainMeshEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CustomEditor(typeof(TerrainMesh))]
+    public class TerrainMeshEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            TerrainMesh target = serializedObject.targetObject as TerrainMesh;
+
+            if (GUILayout.Button("Regenerate"))
+            {
+                target.Regenerate();
+            }
+        }
+    }
+}
--- Assets/Scripts/TerrainMesh.cs
+++ Assets/Scripts/TerrainMesh.cs
@@ -2,19 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainMesh : MonoBehaviour 
+public class TerrainMesh : MonoBehaviour
 {
+    [SerializeField] private float _length = 100;
+    [SerializeField] private float _width = 100;
+    [SerializeField] private int _fractalMaxTimes = 6;
+    [SerializeField] private bool _autoRotate = true;
+    [SerializeField] private float _rotateSpeed = 10;
+    
     private MeshFilter meshFilter;
 
     void Awake()
     {
         meshFilter = GetComponentInChildren<MeshFilter>();
+        Regenerate();
+    }
+
+    public void Regenerate()
+    {
+        if (meshFilter.mesh)
+        {
+            Destroy(meshFilter.mesh);
+        }
+        
+        meshFilter.transform.localPosition = new Vector3(-0.5f * _length, 0, -0.5f * _width);
 
         Mesh mesh = new Mesh();
         meshFilter.mesh = mesh;
 
         FractalTerrain terrain = new FractalTerrain();
-        terrain.Init(100, 100, Random.Range(0, 20140413), 6);
+        terrain.Init(_length, _width, Random.Range(0, 20140413), _fractalMaxTimes);
         terrain.Generate();
 
         mesh.vertices = terrain.VertexList.ToArray();
@@ -25,6 +42,12 @@ public class TerrainMesh : MonoBehaviour
         meshFilter.GetComponent<Renderer>().sharedMaterial.SetTexture("_TerrainMap", terrain.TerrainMap);
     }
 
-
+    void Update()
+    {
+        if (_autoRotate)
+        {
+            transform.localEulerAngles += _rotateSpeed * Vector3.up * Time.deltaTime;
+        }
+    }
 
 }
